b6e80eca56a99d769cab73188a974d13745ef10e
Wesley <122568999+wesleymethorst@users.noreply.github.com>
2025-02-25 12:57:52 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,81 @@
+const dieren = [];
+const dierenLijst = [];
+let huidigDier = null;
+let correct = 0;
+let wrong = 0;
+
+// VERKRIJG ALLE DATA OVER DIEREN
+fetch('dieren.json')
+    .then(response => response.json())
+    .then(data => {   
+        dieren.push(...data);
+        laadWillekeurigDier()
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const animalInput = document.querySelector(".input");
+
+    // animalInput.addEventListener("input", function() {
+    //     controleerInput(animalInput.value);
+    // });
+
+    animalInput.addEventListener('keypress', function(event) {
+        if (event.key === 'Enter') {
+            controleerInput(animalInput.value);
+            event.target.value = "";
+        };
+    })
+});
+
+function laadWillekeurigDier() {
+    const dierenContainer = document.querySelector(".plaatjes-container");
+    const weetjesTekst = document.querySelector(".weetjes-tekst");
+
+    if (dierenLijst.length >= dieren.length) {
+        console.log("Alle dieren zijn getoond.");
+        return;
+    }
+
+    let randomNummer = createRandomNumber();
+    while (dierenLijst.includes(randomNummer)) {
+        randomNummer = createRandomNumber();
+    }
+
+    dierenLijst.push(randomNummer);
+    huidigDier = dieren[randomNummer];  // Opslaan in de globale variabele
+    dierenContainer.src = `images/${huidigDier.number}.jpg`;
+    weetjesTekst.textContent = huidigDier.weetje;
+}
+
+
+function updateCorrect() {
+    correct++;
+    document.getElementById("correctScore").textContent = correct;
+  }
+  
+  function updateWrong() {
+    wrong++;
+    document.getElementById("wrongScore").textContent = wrong;
+  }
+
+// CONTROLEER INPUT TEXTBOX
+function controleerInput(input) {
+    if (normalizeString(input) === normalizeString(huidigDier.naam)) {
+        updateCorrect();
+    } else {
+        updateWrong();
+    }
+
+    laadWillekeurigDier();
+}
+
+function normalizeString(str) {
+    return str
+      .toLowerCase()                        // ZET DE TEKST OM NAAR KLEINE LETTERS
+      .normalize("NFD")                     // SPLITS LETTERS EN DIAKRITISCHE TEKENS
+      .replace(/[\u0300-\u036f]/g, "");      // VERWIJDER DE DIAKRITISCHE TEKENS
+}
+
+function createRandomNumber() {
+    return Math.floor(Math.random() * dieren.length)
+}
