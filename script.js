4f31272901e6c6faa5e19a86c89e7d7bc080ec52
wesleymethorst <wesley.methorst@gmail.com>
2025-02-28 11:26:30 +0100

    Remove MongoDB connection code from script.js

--- script.js
+++ script.js
@@ -4,9 +4,6 @@ let huidigDier = null;
 let correct = 0;
 let wrong = 0;
 let playerName = "";
-const uri = process.env.MONGODB_URI;
-const client = new MongoClient(uri);
-await client.connect();
 
 
 // VERKRIJG ALLE DATA OVER DIEREN
